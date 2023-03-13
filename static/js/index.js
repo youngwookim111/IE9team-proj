3e776cc4bfa842053f790ba1936bf63e40a87237
Chaeyeon Seo <ssallyseo@gmail.com>
2023-03-14 02:48:51 +0900

    modify js
    detail page new tab --> pop up

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,21 @@
+// 프로필사진 클릭 시 각 멤버 상세페이지로 이동 (팝업으로 띄우기)
+function go_detail_page(name) {
 
-// 프로필사진 클릭 시 각 멤버 상세페이지로 이동
-function go_detail_page(name){
-    window.open(`${name}`, '_blank');
+    //창 크기 지정
+    let width = 1200;
+    let height = 900;
+    //pc화면기준 가운데 정렬
+    let left = (window.screen.width / 2) - (width / 2);
+    let top = (window.screen.height / 4);
+
+    //윈도우 속성
+    let windowStatus = 'width=' + width + ', height=' + height + ', left=' + left + ', top=' + top + ', scrollbars=yes, status=yes, resizable=yes, titlebar=yes';
+
+    //연결하고싶은url
+    const url = `${name}`;
+
+    //등록된 url 및 window 속성 기준으로 팝업창을 연다.
+    window.open(url, "hello popup", windowStatus);
+
+    // window.open(`${name}`, '_blank');
 }
